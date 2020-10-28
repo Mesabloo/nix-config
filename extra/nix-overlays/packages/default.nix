fe363516bf246ddd0e17c161b5521e1a2c4d63f1
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-10-28 15:28:02 +0100

    key-mon: import python application from github

--- extra/nix-overlays/packages/default.nix
+++ extra/nix-overlays/packages/default.nix
@@ -5,6 +5,7 @@ let
 in
 {
   deadd-notification-center = pkgs.callPackage ./deadd-notification-center.nix { inherit pkgs; };
+  key-mon = pkgs.callPackage ./key-mon.nix { inherit pkgs; };
   neuron = pkgs.callPackage ./neuron.nix { inherit pkgs; };
   mount-helper = pkgs.callPackage ./mount-helper.nix { inherit pkgs; };
   polybar-scripts = pkgs.callPackages ./polybar-scripts { inherit pkgs; };
