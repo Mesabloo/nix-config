06a7cc2548908130e8bfdb2ab4a483742b109543
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-11-07 14:57:35 +0100

    compton: add some fading to the config

--- config/compton.nix
+++ config/compton.nix
@@ -3,6 +3,21 @@
 with lib;
 {
   config = mkIf config.modules.services.compton.enable {
-    services.picom.shadow = true;
+    services.picom = {
+      # Shadows
+      shadow = true;
+
+
+
+      # Fading
+      fade = true;
+      fadeDelta = 5;
+      fadeSteps = [ "0.028" "0.03" ];
+
+      backend = "xrender";
+      # experimentalBackends = true;
+      refreshRate = 75;
+
+    };
   };
 }
