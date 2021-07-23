bdf0c05805142109aa38008eb4b3eb14b5352669
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-07-23 23:39:00 +0200

    polybar: parameterize system bar with environment variables for network interfaces

--- config/polybar/xmonad/launch.sh
+++ config/polybar/xmonad/launch.sh
@@ -3,13 +3,16 @@
 # Terminate already running bar instances
 killall -r 'polybar*' -w
 
+WIFI=$(ip link | awk -F'( |:)' '$3 ~ /wl.*/ {print $3}')
+ETHE=$(ip link | awk -F'( |:)' '$3 ~ /enp.*/ {print $3}')
+
 if type 'xrandr' &> /dev/null; then
   SCREEN_COUNT=$(xrandr --listactivemonitors | grep 'Monitors:' | awk '{print $2}')
 
   case $SCREEN_COUNT in
     1)
       polybar -c '##POLYBAR-WMINFO##' wminfo &
-      polybar -c '##POLYBAR-SYSINFO##' systeminfo-bottom &
+      WIFI_IF="$WIFI" ETHER_IF="$ETHE" polybar -c '##POLYBAR-SYSINFO##' systeminfo-bottom &
     ;;
     *)
       # Let's assume that the screen numbers represent their order from left to right
@@ -18,11 +21,11 @@ if type 'xrandr' &> /dev/null; then
       SCREEN_N=${SCREENS_INFO[-1]}
 
       MONITOR=$SCREEN_0 polybar -c '##POLYBAR-WMINFO##' wminfo &
-      MONITOR=$SCREEN_N polybar -c '##POLYBAR-SYSINFO##' systeminfo-top &
+      WIFI_IF="$WIFI" ETHER_IF="$ETHE" MONITOR=$SCREEN_N polybar -c '##POLYBAR-SYSINFO##' systeminfo-top &
     ;;
   esac
 else
   # Assuming only one screen...
   polybar -c '##POLYBAR-WMINFO##' wminfo &
-  polybar -c '##POLYBAR-SYSINFO##' systeminfo-bottom &
+  WIFI_IF="$WIFI" ETHER_IF="$ETHE" polybar -c '##POLYBAR-SYSINFO##' systeminfo-bottom &
 fi
