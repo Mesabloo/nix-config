7b168f718c49a71c4ec9cda9c4ed02cd46554f08
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-08-17 14:52:17 +0200

    autorandr: redirect polybar output to /dev/null

--- config/autorandr.nix
+++ config/autorandr.nix
@@ -5,9 +5,9 @@ with lib;
   config = mkIf config.modules.services.autorandr.enable {
     programs.autorandr.hooks = {
       postswitch = mkIf config.modules.services.polybar.enable {
-        "reload-polybar" = "${config.xdg.configHome}/polybar/launch.sh";
+        "reload-polybar" = "${config.xdg.configHome}/polybar/launch.sh &> /dev/null";
       } // {
-        "restore-background" = "${pkgs.nitrogen}/bin/nitrogen --restore &";
+        "restore-background" = "${pkgs.nitrogen}/bin/nitrogen --restore";
       };
     };
   };
