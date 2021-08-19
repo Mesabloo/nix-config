e6bf5105f6d1ef02e3e67b34791663d3e63ab559
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-08-19 15:42:56 +0200

    autorandr: also restore the wallpaper on post switch

--- config/autorandr.nix
+++ config/autorandr.nix
@@ -4,11 +4,21 @@ with lib;
 {
   config = mkIf config.modules.services.autorandr.enable {
     programs.autorandr.hooks = {
-      postswitch = mkIf config.modules.services.polybar.enable {
-        "reload-polybar" = "${config.xdg.configHome}/polybar/launch.sh &> /dev/null";
-      } // {
-        "restore-background" = "${pkgs.nitrogen}/bin/nitrogen --restore";
-      };
+      postswitch =
+        let reload-polybar = if config.modules.services.polybar.enable then {
+              "reload-polybar" = ''
+                echo "Reloading all polybars..."
+                ${config.xdg.configHome}/polybar/launch.sh &> /dev/null
+              '';
+            } else {};
+            reload-background = {
+              "restore-background" = ''
+                echo "Reloading wallpaper"
+                ${pkgs.nitrogen}/bin/nitrogen --restore
+              '';
+            };
+        in
+          reload-polybar // reload-background;
     };
   };
 }
